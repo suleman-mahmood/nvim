cc0b57ffb85f3dc9a13fe092512ccbcda9cf3dc8
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-04-07 10:02:30 +0500

    lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -5,8 +5,22 @@ return {
     opts = {
       ---@type lspconfig.options
       servers = {
+        pyright = {
+          settings = {
+            python = {
+              pythonPath = "/Users/sulemlo/Library/Caches/pypoetry/virtualenvs/mazlo-0I5QZlEV-py3.13/bin/python",
+            },
+          },
+        },
+        ruff = {},
+        dartls = {
+          settings = {
+            dart = {
+              lineLength = 100,
+            },
+          },
+        },
         tsserver = {},
-        dartls = {},
       },
     },
   },
