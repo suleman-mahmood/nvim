ae490dfc4eb5f691bd40eca4a2e06e05b367f333
Suleman Mahmood <sulemanmahmood99@gmail.com>
2025-04-10 08:09:42 +0500

    include sql file

--- lua/custom/snippets/init.lua
+++ lua/custom/snippets/init.lua
@@ -3,5 +3,6 @@ local M = {}
 M.dart = require("custom.snippets.dart")
 M.python = require("custom.snippets.python")
 M.rust = require("custom.snippets.rust")
+M.rust = require("custom.snippets.sql")
 
 return M
